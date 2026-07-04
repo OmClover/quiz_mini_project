018df7fa219b57f8b0bf4ef7b34b2bdfe975cb72
OmClover <om.chaudhari@cloverinfotech.com>
2026-07-04 15:45:54 +0530

    complete mini project of quiz

--- quiz.py
+++ quiz.py
@@ -1,34 +1,40 @@
 username = input("Enter your name: ")
-choice = input("Are you ready to attempt the quiz? y/n: ")
 questions = ["What is the closest planet to the Sun?","Which mammal is known to have the most powerful bite?","What is the smallest ocean in the world?"]
 all_options = [["Venus","Mars","Mercury","Earth"],["Grizzly Bear","Hippopotamus","Lion","Alligator"],["Indian Ocean","Pacific Ocean","Arctic Ocean","Atlantic Ocean"]]
-letters = ["A","B","C","D"]
+letters = ["a","b","c","d"]
+answers_list = ["c","b","c"]
 # score = 0
 is_continue = True
 
+choice = input("Are you ready to attempt the quiz? y/n: ").lower()
+
 while is_continue:
-    if choice == "y" or "":
+    if choice == "y" or choice == "":
         score = 0
-        for i in range (3):
+        for i in range (len(questions)):
                 print(f"\nQ{i+1}. {questions[i]}")
                 current = all_options[i]
-                for j in range (4):
-                     print(f"{letters[j]}. {current[j]}")
-                answer = input("Enter correct option: ")
-                if answer.lower() == "a":
-                     score +=1
-                else:
-                     score = score
+                for j in range (len(current)):
+                     print(f"{letters[j].upper()}. {current[j]}")
+                while True:
+                    answer = input("Enter correct option: ").lower()
+                    if answer in letters:
+                         if answer == answers_list[i]:
+                              score +=1
+                         else:
+                              pass
+                         break
+                    else:
+                         print("Invalid choice!!!, Please enter choice A,B,C or D")
         print("\n\n")
         print("="*40)
         print(f"Thanks {username} for attempting the quiz!\n")
-        print(f"Your score is {score}/3 and Percentage is {(score/3)*100}")
+        print(f"Your score is {score}/{len(questions)} and Percentage is {(score/(len(questions)))*100}")
         continue_ = input("\nDo you want to try again? y/n: ")
         if continue_.lower() == "y":
              is_continue = True
-        elif continue_.lower() == "n":
-             is_continue = False
         else:
+             print("Okay, Good Bye!")
              is_continue = False
     elif choice == "n":
         print("Okay, Good Bye!")
